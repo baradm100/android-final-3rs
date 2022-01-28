cea16c1b07bc0b7ed8896bc875eddc54a27d6e15
Bar Admoni <badmoni@cisco.com>
2022-01-28 18:36:36 +0200

    Finilize login!

--- app/src/main/java/com/colman/bar/admoni/a3rs/FeedActivity.java
+++ app/src/main/java/com/colman/bar/admoni/a3rs/FeedActivity.java
@@ -1,10 +1,13 @@
 package com.colman.bar.admoni.a3rs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,7 +26,17 @@ public class FeedActivity extends AppCompatActivity {
             return;
         }
         TextView welcomeTextView = findViewById(R.id.wellcomeMessage);
+        Button logoffButton = findViewById(R.id.logoffButton);
 
         welcomeTextView.setText(currentUser.getEmail());
+        logoffButton.setOnClickListener(this::handleLogoff);
     }
+
+    private void handleLogoff(View v) {
+        mAuth.signOut();
+        Intent i = new Intent(this, MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
+    }
+
 }
--- app/src/main/java/com/colman/bar/admoni/a3rs/MainActivity.java
+++ app/src/main/java/com/colman/bar/admoni/a3rs/MainActivity.java
@@ -3,8 +3,11 @@ package com.colman.bar.admoni.a3rs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,12 +34,20 @@ public class MainActivity extends AppCompatActivity {
 
         EditText emailTextEdit = findViewById(R.id.emailEditText);
         EditText passwordEditText = findViewById(R.id.passwordEditText);
+        ProgressBar loginProgressBar = findViewById(R.id.loginProgressBar);
 
         Button loginButton = findViewById(R.id.loginButton);
+        Button registerButton = findViewById(R.id.registerButton);
+
         loginButton.setOnClickListener(v -> {
-            Log.d(Consts.TAG, "Login clicked!");
+            if (!validateForm(emailTextEdit, passwordEditText)) {
+                return;
+            }
+
             String email = emailTextEdit.getText().toString();
             String password = passwordEditText.getText().toString();
+
+            showLoading(emailTextEdit, passwordEditText, loginProgressBar, loginButton, registerButton);
 
             mAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, task -> {
@@ -49,16 +60,21 @@ public class MainActivity extends AppCompatActivity {
                             Log.w(Consts.TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
+                            hideLoading(emailTextEdit, passwordEditText, loginProgressBar, loginButton, registerButton);
                         }
                     });
         });
 
-        Button registerButton = findViewById(R.id.registerButton);
         registerButton.setOnClickListener(v -> {
+            if (!validateForm(emailTextEdit, passwordEditText)) {
+                return;
+            }
+
             String email = emailTextEdit.getText().toString();
             String password = passwordEditText.getText().toString();
 
-            Log.d(Consts.TAG, "Register clicked! " + email + " " + password);
+            showLoading(emailTextEdit, passwordEditText, loginProgressBar, loginButton, registerButton);
+
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, task -> {
                         if (task.isSuccessful()) {
@@ -70,15 +86,60 @@ public class MainActivity extends AppCompatActivity {
                             Log.w(Consts.TAG, "createUserWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
+                            hideLoading(emailTextEdit, passwordEditText, loginProgressBar, loginButton, registerButton);
                         }
                     });
         });
+    }
+
+    private boolean validateForm(EditText emailTextEdit, EditText passwordEditText) {
+        boolean isValid = true;
+
+        if (emailTextEdit.getText().toString().trim().length() == 0) {
+            emailTextEdit.setError("Email is a required field");
+            isValid = false;
+            emailTextEdit.requestFocus();
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailTextEdit.getText().toString()).matches()) {
+            emailTextEdit.setError("Email must be in a valid format");
+            isValid = false;
+            emailTextEdit.requestFocus();
+        }
+
+        if (passwordEditText.getText().toString().trim().length() == 0) {
+            passwordEditText.setError("Password is a required field");
+            if (isValid) passwordEditText.requestFocus();
+            isValid = false;
+        }
+
+
+        return isValid;
+    }
+
+    private void showLoading(EditText emailTextEdit, EditText passwordEditText, ProgressBar loginProgressBar, Button loginButton, Button registerButton) {
+        emailTextEdit.setEnabled(false);
+        passwordEditText.setEnabled(false);
+        loginProgressBar.setVisibility(View.VISIBLE);
+        loginButton.setEnabled(false);
+        loginButton.setVisibility(View.INVISIBLE);
+        registerButton.setEnabled(false);
+        registerButton.setVisibility(View.INVISIBLE);
+    }
+
+    private void hideLoading(EditText emailTextEdit, EditText passwordEditText, ProgressBar loginProgressBar, Button loginButton, Button registerButton) {
+        emailTextEdit.setEnabled(true);
+        passwordEditText.setEnabled(true);
+        loginProgressBar.setVisibility(View.INVISIBLE);
+        loginButton.setEnabled(true);
+        loginButton.setVisibility(View.VISIBLE);
+        registerButton.setEnabled(true);
+        registerButton.setVisibility(View.VISIBLE);
     }
 
     private void moveToFeed() {
         Intent i = new Intent(this, FeedActivity.class);
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(i);
-        finish();
     }
 }
