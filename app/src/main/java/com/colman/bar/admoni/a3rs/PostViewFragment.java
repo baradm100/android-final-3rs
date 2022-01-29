6745afb67a3a74eb3d6be5bd8a3995cd3983c33a
Bar Admoni <badmoni@cisco.com>
2022-01-29 22:38:02 +0200

    Open post page

--- app/src/main/java/com/colman/bar/admoni/a3rs/PostViewFragment.java
+++ app/src/main/java/com/colman/bar/admoni/a3rs/PostViewFragment.java
@@ -4,54 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.colman.bar.admoni.a3rs.models.Post;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PostViewFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class PostViewFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final String ARG_POST_ID = "postId";
+    public static final String ARG_POST = "post";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+    private String postId;
+    private Post post;
 
     public PostViewFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PostViewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PostViewFragment newInstance(String param1, String param2) {
-        PostViewFragment fragment = new PostViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            postId = getArguments().getString(ARG_POST_ID);
+            post = (Post) getArguments().getSerializable(ARG_POST);
         }
     }
 
@@ -61,6 +43,10 @@ public class PostViewFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_post_view, container, false);
         v.findViewById(R.id.goBackToFeedButton).setOnClickListener(this::handleGoBackToFeedClick);
+        TextView textEdit = v.findViewById(R.id.postViewTextView);
+        if (post != null) {
+            textEdit.setText(post.getTitle() + " - " + post.getSubTitle());
+        }
         return v;
     }
 
--- app/src/main/java/com/colman/bar/admoni/a3rs/models/Post.java
+++ app/src/main/java/com/colman/bar/admoni/a3rs/models/Post.java
@@ -1,9 +1,10 @@
 package com.colman.bar.admoni.a3rs.models;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Post {
+public class Post implements Serializable {
     private String title;
     private String subTitle;
     private String description;
